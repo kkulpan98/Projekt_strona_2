b07704243989a1213f27319390b20c01f0d4150a
kamkul <kkulpan98@gmail.com>
2019-12-08 20:23:06 +0100

    przerobiono walidacje z js na jqeury

--- jq/jquery.js
+++ jq/jquery.js
@@ -141,3 +141,61 @@ function prepareCalendar() {
         calendar.render();
     });
 }
+
+function checkString(str, msg) {
+    if (isWhiteSpace(str) || isEmpty(str)) {
+        return msg + ", ";
+    } else return "";
+}
+
+function validate() {
+    if (isEmpty(isValid())) {
+        return true;
+    } else {
+        $("#form_error").html(isValid());
+        console.log(isValid());
+        startTimer();
+        return false;
+    }
+}
+
+function startTimer() {
+    window.setTimeout("clearError()", 5000);
+}
+
+function clearError() {
+    $("#form_error").html("");
+}
+
+
+function isValid() {
+    return checkString($("#firstname").val(), "Imie żle") +
+        checkString($("#lastname").val(), "Nazwisko źle ") +
+        checkPhoneNumberRegEx($("#phone_number").val()) +
+        checkEmailRegEx($("#email").val());
+}
+
+function checkEmailRegEx(str) {
+    var email = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+    if (email.test(str)) return ""; else return (" Podaj właściwy e-mail");
+}
+
+function checkPhoneNumberRegEx(str) {
+    var number = /^(\+{1}\d{2,3}\s?[(]{1}\d{1,3}[)]{1}\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}$/;
+    if (number.test(str)) return ""; else return (" Podaj właściwy numer");
+}
+
+function isWhiteSpace(str) {
+    var ws = "\t\n\r ";
+    for (let i = 0; i < str.length; i++) {
+        var c = str.charAt(i);
+        if (ws.indexOf(c) > -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isEmpty(str) {
+    return (!str || str.length === 0);
+}
